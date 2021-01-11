9805a53321f1a95632a4c1a8a52043bd840500dd
toxipol <75957068+toxipol@users.noreply.github.com>
2021-01-11 13:59:00 +0200

    1

---
+++ js/init.js
@@ -0,0 +1,8 @@
+$(function() {
+    $('a[href^=""]').click(function (){
+        var elementClick = $(this).attr("href");
+        var destination = $(elementClick).offset().top - 60;
+        jQuery("html:not(:animated), body:not(:animated)").animate({scrollTop: destination}, 800);
+        return false;
+    });
+});  
